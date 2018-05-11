2299ad9851187ec3b4e95363f669c510c7eb49e8
Dmitry-Vasilyev <dimav.programming@gmail.com>
2018-05-11 14:29:21 +0300

    Count Element Occurence

---
+++ Binary Search/IB_CountElementOccurence/Main.java
@@ -0,0 +1,10 @@
+package ProgrammingExercises.BinarySearch.IB_CountElementOccurence;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {1, 2, 2, 2, 2, 3, 3, 4, 5, 6};
+        System.out.println(solution.findCount(arr, 2));
+    }
+}
