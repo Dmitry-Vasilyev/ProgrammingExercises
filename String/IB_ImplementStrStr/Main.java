ab930fe08b23ab257606ce9907e4865e052aa45a
DimavLinux <dimav.programming@gmail.com>
2018-06-25 12:48:01 +0300

    IB_Implement StrStr

--- String/IB_ImplementStrStr/Main.java
+++ String/IB_ImplementStrStr/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.strStr("HellBoy", "Boy"));
+        System.out.println(solution.strStr("bbbbbbbbab", "baba"));
 
     }
 }
--- String/IB_ImplementStrStr/Solution.java
+++ String/IB_ImplementStrStr/Solution.java
@@ -7,15 +7,42 @@ https://www.interviewbit.com/problems/implement-strstr/
 
 public class Solution {
     public int strStr(final String A, final String B) {
-        if(A.length() == 0 || B.length() == 0) return -1;
-        int counter = 0;
-        for(int i = 0; i < A.length() - B.length() + 1; i++) {
-            while(A.charAt(i + counter) == B.charAt(counter)) {
-                counter++;
-                if(counter == B.length()) return i;
+        int pi[] = getPi(B);
+        int i = 0;
+        int j = 0;
+        while (i < A.length()){
+            if(A.charAt(i) == B.charAt(j)) {
+                i++;
+                j++;
+                if(j == B.length()) return i - j;
+            } else if(j == 0){
+                i++;
+                if(i == A.length()) return -1;
+            } else {
+                j = pi[j-1];
             }
-            counter = 0;
         }
         return -1;
+    }
+
+    private int[] getPi(String A) {
+        int[] pi = new int[A.length()];
+        pi[0] = 0;
+        int i = 1;
+        int j = 0;
+
+        while(i < A.length()) {
+            if(A.charAt(i) == A.charAt(j)) {
+                pi[i] = j+1;
+                j++;
+                i++;
+            } else {
+                pi[i] = 0;
+                i++;
+                if(j == 0) continue;
+                j = pi[j-1];
+            }
+        }
+        return pi;
     }
 }
